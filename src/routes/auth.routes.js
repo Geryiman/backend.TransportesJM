a003550dd3b6fc3d0ded5180581ce28879287374
Geryiman <geryiman0@gmail.com>
2025-07-31 12:26:34 -0600

    Update auth.routes.js

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -6,7 +6,6 @@ const verifyToken = require('../middlewares/verifyToken');
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
-router.get('/perfil/:id', authController.getPerfil);
 router.put('/cambiar-contrasena/', authController.cambiarContrasena);
 
 router.get('/reservas/:id', authController.obtenerReservasUsuario);
