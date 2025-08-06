571d7d367e9c7c11e65f6ffe83ba00740817e250
Geryiman <geryiman0@gmail.com>
2025-08-06 01:46:09 -0600

    Update cuentasViaje.routes.js

--- src/routes/cuentasViaje.routes.js
+++ src/routes/cuentasViaje.routes.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/cuentasViaje.controller');
 
-router.get('/:id_viaje/resumen', controller.obtenerResumen);
+router.get('/:id/resumen', controller.obtenerResumenCuenta); // ✅ correcto
+
 router.post('/:id_viaje', controller.guardarCuenta);
 
 module.exports = router;
